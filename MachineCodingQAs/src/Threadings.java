ea602495d5fba67e93d1aa12edee4fa0fc2ff0dc
udayqizy01 <uday.kiran@qizy.in>
2026-01-22 22:47:34 +0530

    threads updates

--- MachineCodingQAs/src/Threadings.java
+++ MachineCodingQAs/src/Threadings.java
@@ -23,19 +23,66 @@ class Worker extends Thread {
     }
 }
 
+//Inter-Thread Communication Example --> producer consumer problem
+
+class resource{
+    int r;
+    public boolean valueSet=false;
+    public synchronized void  setR(int r){
+        while(valueSet){
+            try{ wait(); }catch(Exception e){}
+        }
+        System.out.println("Set :"+r);
+        this.r=r;
+        valueSet=true;
+        notify();
+    }
+    public synchronized void getR(){
+        while(!valueSet){
+            try{ wait(); }catch(Exception e){}
+        }
+        System.out.println("Get : "+r);
+        valueSet=false;
+        notify();
+    }
+}
+
+class Producer extends Thread{
+    resource r;
+    Producer(resource r){
+        this.r=r;
+    }
+    public void  run(){
+        int i=0;
+        while(true){
+            r.setR(i++);
+            try{Thread.sleep(1000);} catch (Exception e){}
+        }
+    }
+}
+
+class Consumer extends  Thread{
+    resource r;
+    Consumer(resource r){
+        this.r=r;
+    }
+    public void run(){
+        while(true){
+            r.getR();
+            try{Thread.sleep(1000);} catch (Exception e){}
+        }
+    }
+}
+
 public class Threadings {
     public static void main(String[] args){
 
         Bank b = new Bank();
         Worker w1 = new Worker(b); // Thread is created..
         Worker w2 = new Worker(b);
-
-
         w1.start(); //thread started..
         w2.start();
-
         try{
-
             w1.join();
             w2.join(); //running concurrently one after another not at that same time on the shared resoruce
         }
@@ -43,6 +90,18 @@ public class Threadings {
             System.out.println(e);
         }
         System.out.println("count is : "+b.getBalance());
+
+
+        resource r = new resource();
+        Producer p = new Producer(r);
+        Consumer c = new Consumer(r);
+
+        p.start();
+        c.start();
+
+
+
+
 
     }
 }
