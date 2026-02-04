11cd2c0035245a67d62bfb687968b928a67b9159
UDAY KIRAN <123919244+Mudaykirann@users.noreply.github.com>
2026-02-04 11:26:31 +0530

    Mapping relations and its examples
    Updated the Main class to include Laptop and Student entities, demonstrating Hibernate mapping relationships.

--- hibernate-first/src/main/java/com/uday/Main.java
+++ hibernate-first/src/main/java/com/uday/Main.java
@@ -13,22 +13,48 @@ public class Main {
     public static void main(String[] args) {
 
         // 1. Initialize the Embeddable object (Value Object)
-        EmployeeName name = new EmployeeName();
-        name.setFname("Ramu");
-        name.setLname("Ram");
-        name.setSname("Mangala");
+//        EmployeeName name = new EmployeeName();
+//        name.setFname("Ramu");
+//        name.setLname("Ram");
+//        name.setSname("Mangala");
+
+
+        // ***Showing how the Mapping relations would be in hibernate *** ;;;
+        Laptop l = new Laptop();
+        l.setLid(101);
+        l.setLname("Dell");
+
+
+        Student s = new Student();
+        s.setId(1);
+        s.setName("Kiran");
+        s.setMarks(90);
+        //adding laptops to the student;
+        s.getL().add(l); // one student got many laptops --> onetomany
+
+        //adding student into the laptops
+        l.getStudent().add(s); // Many laptops for the many students --> manytomany
+
+
 
         // 2. Initialize the Entity object
-        Employee emp = new Employee();
-        emp.setId(2408);
-        emp.setName(name); // Mapping the embedded object to the entity
-        emp.setDesignation("Consultant");
+//        Employee emp = new Employee();
+//        emp.setId(2408);
+//        emp.setName(name); // Mapping the embedded object to the entity
+//        emp.setDesignation("Consultant");
 
         // 3. Configuration & SessionFactory setup
         // addAnnotatedClass tells Hibernate to scan the Employee class for JPA annotations
         // configure() loads settings from hibernate.cfg.xml
+//        SessionFactory factory = new Configuration()
+//                .addAnnotatedClass(com.uday.Employee.class)
+//                .configure("hibernate.cfg.xml")
+//                .buildSessionFactory();
+
+
         SessionFactory factory = new Configuration()
-                .addAnnotatedClass(com.uday.Employee.class)
+               .addAnnotatedClass(com.uday.Laptop.class)
+               .addAnnotatedClass(com.uday.Student.class)
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
 
@@ -42,7 +68,12 @@ public class Main {
             /* --- JPA COMPLIANT CRUD OPERATIONS --- */
 
             // CREATE: persist() makes the object managed and schedules it for insert
-            session.persist(emp);
+            //session.persist(emp);
+
+
+            //for lotp and student calss
+            session.persist(l);
+            session.persist(s);
 
             // UPDATE: merge() is used to save changes to an existing record
             // session.merge(emp);
