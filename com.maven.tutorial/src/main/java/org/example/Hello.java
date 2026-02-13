ac83889e4886ecccd454e556bee8018a6a3cb6ba
UDAY KIRAN <123919244+Mudaykirann@users.noreply.github.com>
2026-02-13 21:31:43 +0530

    Update Hello.java

--- com.maven.tutorial/src/main/java/org/example/Hello.java
+++ com.maven.tutorial/src/main/java/org/example/Hello.java
@@ -3,5 +3,8 @@ package org.example;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hellow WOrdl");
+
+        //example to show how to run the maven commands mvn compile , build and test
     }
 }
+
